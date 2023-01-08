4b39913438021487414ae96e51ccd5c182978783
Lance T. Erickson <lancetarn@gmail.com>
2023-01-07 23:08:00 -0600

    Pong

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import os
+from random import randint
+from time import sleep
+
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 SERVICE_NAME = os.getenv("SERVICE_NAME")
 OTHER_NAME = os.getenv("OTHER_NAME")
+OTHER = f"http://{OTHER_NAME}:8000"
 
 
 @app.route("/")
 def ping():
-    return "pong"
+    if randint(0, 9) == 5:
+        raise RuntimeError("Whiff!")
+    sleep(0.005)
+    res = requests.get(OTHER)
+    return res.text
